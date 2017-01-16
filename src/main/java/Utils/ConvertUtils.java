7bf12e2696d5c1df99a52742dea8f64a7ba56991
pj <pj@nbct.com.cn>
2017-01-16 14:00:08 +0800

    a

--- src/main/java/Utils/ConvertUtils.java
+++ src/main/java/Utils/ConvertUtils.java
@@ -1,13 +1,28 @@
 package Utils;
 
-
+import it.sauronsoftware.jave.AudioAttributes;
+import it.sauronsoftware.jave.Encoder;
+import it.sauronsoftware.jave.EncodingAttributes;
 
 import java.io.File;
 
 public class ConvertUtils {
 	
 	  public static void changeToMp3(String sourcePath, String targetPath) {  
-	        
+	        File source = new File(sourcePath);  
+	        File target = new File(targetPath);  
+	        AudioAttributes audio = new AudioAttributes();  
+	        Encoder encoder = new Encoder();  
+	  
+	        audio.setCodec("libmp3lame");  
+	        EncodingAttributes attrs = new EncodingAttributes();  
+	        attrs.setFormat("mp3");  
+	        attrs.setAudioAttributes(audio);  
+	        try {  
+	            encoder.encode(source, target, attrs);  
+	        } catch (Exception e) { 
+	        	//有错误,但能转换成功
+	        }
 	    } 
 	   
 	  /**
