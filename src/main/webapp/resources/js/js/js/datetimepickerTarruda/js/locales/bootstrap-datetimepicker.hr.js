31c94bcbf28dd7b2d5dc43226e14e30049c44082
Sun <sundw@nbct.com.cn>
2017-02-06 12:42:32 +0800

    others

--- src/main/webapp/resources/js/js/js/datetimepickerTarruda/js/locales/bootstrap-datetimepicker.hr.js
+++ src/main/webapp/resources/js/js/js/datetimepickerTarruda/js/locales/bootstrap-datetimepicker.hr.js
@@ -1,13 +1,1 @@
-/**
- * Croatian localisation
- */
-;(function($){
-	$.fn.datetimepicker.dates['hr'] = {
-		days: ["Nedjelja", "Ponedjelja", "Utorak", "Srijeda", "ÄŒetrtak", "Petak", "Subota", "Nedjelja"],
-		daysShort: ["Ned", "Pon", "Uto", "Srr", "ÄŒet", "Pet", "Sub", "Ned"],
-		daysMin: ["Ne", "Po", "Ut", "Sr", "ÄŒe", "Pe", "Su", "Ne"],
-		months: ["SijeÄanj", "VeljaÄa", "OÅ¾ujak", "Travanj", "Svibanj", "Lipanj", "Srpanj", "Kolovoz", "Rujan", "Listopad", "Studeni", "Prosinac"],
-		monthsShort: ["Sije", "Velj", "OÅ¾u", "Tra", "Svi", "Lip", "Jul", "Kol", "Ruj", "Lis", "Stu", "Pro"],
-		today: "Danas"
-	};
-}(jQuery));
+ÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀPÉÆ€úÀ
